c6a4b1be02711c18251117a059468cdbb33a9080
Arpit <arjunarora2324@gmail.com>
2021-04-24 12:22:59 +0530

    added_view_file

---
+++ dashboard/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from user.models import *
+# from models import job_opening
+# Create your views here.
+
+def applicantdashboard(request):    
+    return render(request,'applicantdashboard.html')
+
+def joblist(request):
+    tag = request.POST.get('skill')
+    
+    job_opening = Job_Opening.objects.filter(skill=tag)
+    # data = job_opening.split(",")
+    # job_opening = Job_Opening.objects.all()
+    # data=job_opening.first()
+    # for i in job_opening:
+    #     data=i
+
+    # data=(str)(data)
+    # data = data.split(",")
+    # print(data)
+    founder = []
+    
+    for job in job_opening:
+        founder.append(list(Company.objects.filter(user_id=job.user_id).values('company_name')))
+    context = {'job_opening': job_opening}
+    return render(request,'joblist.html',{'job_opening': job_opening, 'founder': founder})
