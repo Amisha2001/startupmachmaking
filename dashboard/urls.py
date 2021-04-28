6ebd1b52fe3ce09c1a14e659f53e7ada50f45050
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2021-04-28 11:35:02 +0530

    View details button configured

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 
     path('applicant', views.applicantdashboard, name="applicantdashboard"),
     path('joblist', views.joblist, name="joblist"),
+    path('<str:slug>',views.jobpost,name = "jobpost"),
     path('send_email', views.send_email, name="send_email"),
 ] 
